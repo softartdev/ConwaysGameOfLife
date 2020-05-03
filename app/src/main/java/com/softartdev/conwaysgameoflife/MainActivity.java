54e9cb72421e265d36183ac3718fecd3c6639483
artur <artik222012@gmail.com>
2020-05-04 04:55:31 +0500

    reformat main activity

--- app/src/main/java/com/softartdev/conwaysgameoflife/MainActivity.java
+++ app/src/main/java/com/softartdev/conwaysgameoflife/MainActivity.java
@@ -1,15 +1,13 @@
 package com.softartdev.conwaysgameoflife;
 
-import android.content.DialogInterface;
-import android.os.Handler;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -17,9 +15,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
     final String NAME_OF_GAME = "Conway's Game of Life";
-    final String SAVE_FILE_EXT = ".life";
     final int LIFE_SIZE = 10;
     boolean[][] lifeGeneration = new boolean[LIFE_SIZE][LIFE_SIZE];
     boolean[][] nextGeneration = new boolean[LIFE_SIZE][LIFE_SIZE];
@@ -33,114 +29,76 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button buttonSave = (Button)findViewById(R.id.buttonSave);
-        Button buttonLoad = (Button)findViewById(R.id.buttonLoad);
-        Button buttonFAQ = (Button)findViewById(R.id.buttonFAQ);
-        final Button buttonStart = (Button)findViewById(R.id.buttonStart);
-        Button buttonStep = (Button)findViewById(R.id.buttonStep);
-        Button buttonRandom = (Button)findViewById(R.id.buttonRandom);
-        Button buttonClean = (Button)findViewById(R.id.buttonClean);
-
-        buttonFAQ.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(MainActivity.this, getString(R.string.rules), Toast.LENGTH_SHORT).show();
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle(getString(R.string.rulesTitle))
-                        .setMessage(getString(R.string.rules))
-                        .setCancelable(false)
-                        .setNegativeButton(getString(R.string.ok),
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-                AlertDialog alert = builder.create();
-                alert.show();
-            }
+        Button buttonSave = findViewById(R.id.buttonSave);
+        Button buttonLoad = findViewById(R.id.buttonLoad);
+        Button buttonFAQ = findViewById(R.id.buttonFAQ);
+        final Button buttonStart = findViewById(R.id.buttonStart);
+        Button buttonStep = findViewById(R.id.buttonStep);
+        Button buttonRandom = findViewById(R.id.buttonRandom);
+        Button buttonClean = findViewById(R.id.buttonClean);
+        buttonFAQ.setOnClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(getString(R.string.rulesTitle))
+                    .setMessage(getString(R.string.rules))
+                    .setCancelable(false)
+                    .setNegativeButton(getString(R.string.ok),
+                            (dialog, id) -> dialog.cancel());
+            AlertDialog alert = builder.create();
+            alert.show();
         });
         buttonSave.setVisibility(View.INVISIBLE);
         buttonLoad.setVisibility(View.INVISIBLE);
-
-        for (int y = 0; y < LIFE_SIZE; y++){
-            for (int x = 0; x < LIFE_SIZE; x++){
+        for (int y = 0; y < LIFE_SIZE; y++) {
+            for (int x = 0; x < LIFE_SIZE; x++) {
                 String cellId = "x" + x + "y" + y;
                 int resID = getResources().getIdentifier(cellId, "id", "com.softartdev.conwaysgameoflife");
-                cellView[x][y] = (ImageView)findViewById(resID);
-
+                cellView[x][y] = findViewById(resID);
                 final int dx = x;
                 final int dy = y;
-                cellView[x][y].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        lifeGeneration[dx][dy] = !lifeGeneration[dx][dy];
-                        repaint(lifeGeneration);
-                    }
+                cellView[x][y].setOnClickListener(view -> {
+                    lifeGeneration[dx][dy] = !lifeGeneration[dx][dy];
+                    repaint(lifeGeneration);
                 });
             }
         }
-
-        buttonRandom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                countGeneration = 1;
-                for (int x = 0; x < LIFE_SIZE; x++) {
-                    for (int y = 0; y < LIFE_SIZE; y++) {
-                        lifeGeneration[x][y] = random.nextBoolean();
-                    }
+        buttonRandom.setOnClickListener(view -> {
+            countGeneration = 1;
+            for (int x = 0; x < LIFE_SIZE; x++) {
+                for (int y = 0; y < LIFE_SIZE; y++) {
+                    lifeGeneration[x][y] = random.nextBoolean();
                 }
-                repaint(lifeGeneration);
             }
+            repaint(lifeGeneration);
         });
-
-        buttonStep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                processOfLife();
-                repaint(nextGeneration);
+        buttonStep.setOnClickListener(view -> {
+            processOfLife();
+            repaint(nextGeneration);
+        });
+        buttonClean.setOnClickListener(view -> {
+            for (int x = 0; x < LIFE_SIZE; x++) {
+                Arrays.fill(lifeGeneration[x], false);
             }
+            repaint(lifeGeneration);
         });
-
-        buttonClean.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                for (int x = 0; x < LIFE_SIZE; x++) {
-                    Arrays.fill(lifeGeneration[x], false);
-                }
-                repaint(lifeGeneration);
-            }
+        buttonStart.setOnClickListener(view -> {
+            buttonStart.setText(goNextGeneration ? getString(R.string.start) : getString(R.string.stop));
+            goNextGeneration = !goNextGeneration;
         });
-
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                buttonStart.setText(goNextGeneration? getString(R.string.start) : getString(R.string.stop));
-                goNextGeneration = !goNextGeneration;
-            }
-        });
-
         final Handler uiHandler = new Handler();
         final TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                if (goNextGeneration){
+                if (goNextGeneration) {
                     processOfLife();
-                    uiHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            repaint(nextGeneration);
-                        }
-                    });
+                    uiHandler.post(() -> repaint(nextGeneration));
                 }
             }
         };
         Timer myTimer = new Timer();
-        myTimer.schedule(timerTask, 0L, (long) showDelay);
-
+        myTimer.schedule(timerTask, 0L, showDelay);
     }
 
-    public void repaint(boolean[][] generation){
+    public void repaint(boolean[][] generation) {
         for (int x = 0; x < LIFE_SIZE; x++) {
             for (int y = 0; y < LIFE_SIZE; y++) {
                 if (generation[x][y]) {
@@ -167,7 +125,9 @@ public class MainActivity extends AppCompatActivity {
                 count += (lifeGeneration[nX][nY]) ? 1 : 0;
             }
         }
-        if (lifeGeneration[x][y]) { count--; }
+        if (lifeGeneration[x][y]) {
+            count--;
+        }
         return count;
     }
 
@@ -178,9 +138,9 @@ public class MainActivity extends AppCompatActivity {
                 int count = countNeighbors(x, y);
                 nextGeneration[x][y] = lifeGeneration[x][y];
                 // if are 3 live neighbors around empty cells - the cell becomes alive
-                nextGeneration[x][y] = (count == 3) ? true : nextGeneration[x][y];
+                nextGeneration[x][y] = (count == 3) || nextGeneration[x][y];
                 // if cell has less than 2 or greater than 3 neighbors - it will be die
-                nextGeneration[x][y] = ((count < 2) || (count > 3)) ? false : nextGeneration[x][y];
+                nextGeneration[x][y] = ((count >= 2) && (count <= 3)) && nextGeneration[x][y];
             }
         }
         for (int x = 0; x < LIFE_SIZE; x++) {
