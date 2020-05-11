5743aec053afe241bfaa0ede252369d0fcef185b
artur <artik222012@gmail.com>
2020-05-11 18:23:06 +0500

    timber

---
+++ app/src/main/java/com/softartdev/conwaysgameoflife/App.java
@@ -0,0 +1,16 @@
+package com.softartdev.conwaysgameoflife;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
