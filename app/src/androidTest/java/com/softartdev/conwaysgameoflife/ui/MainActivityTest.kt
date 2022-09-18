f677e31b21f84ce85d128aa91077c89e28eab1ae
Artur Babichev <artik222012@gmail.com>
2022-09-19 03:52:23 +0400

    fix leak detection in tests

--- app/src/androidTest/java/com/softartdev/conwaysgameoflife/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/softartdev/conwaysgameoflife/ui/MainActivityTest.kt
@@ -6,22 +6,26 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import com.softartdev.conwaysgameoflife.R
+import leakcanary.DetectLeaksAfterTestSuccess
+import leakcanary.TestDescriptionHolder
 import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @Rule
-    @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    @get:Rule
+    val rules: RuleChain = RuleChain.outerRule(TestDescriptionHolder)
+        .around(DetectLeaksAfterTestSuccess())
+        .around(ActivityScenarioRule(MainActivity::class.java))
 
     @Test
     fun mainActivityTest() {
